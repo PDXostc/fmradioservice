b2a8d4a05c442a590f56f9ee76b3daec8f784808
Chris Lamberson <clamberson@gmail.com>
2016-02-18 12:55:12 -0800

    removes test case

--- gst-sdr-j-fm/tests/tune.sh
+++ gst-sdr-j-fm/tests/tune.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 #exec ./tune --gst-debug=srdjrm:5 --gst-debug-level=5  --gst-debug-no-color
-exec ./tune --gst-debug=srdjrm:8 --gst-debug-no-color
+#exec ./tune --gst-debug=srdjrm:8 --gst-debug-no-color
